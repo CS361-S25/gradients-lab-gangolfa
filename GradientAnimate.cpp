9ec3cfb9b402fdb4798add425d12d20b3d38f27b
Hilly Gangolf <gangolfa@carleton.edu>
2025-04-11 09:30:46 -0500

    notes

---
+++ GradientAnimate.cpp
@@ -0,0 +1,53 @@
+#include "emp/web/Animate.hpp"
+#include "emp/web/web.hpp"
+
+emp::web::Document doc{"target"};
+
+class GradientAnimator : public emp::web::Animate
+{
+    emp::web::Canvas canvas{500, 500, "canvas"};
+    double number = 0.0;
+    // Instance variable that is a vector of vectors of floats
+    std::vector<std::vector<float>> cells;
+    const int num_w_boxes = 10;
+    const int num_h_boxes = 10;
+    const int rect_len = 50;
+
+    // Class constructor
+public:
+    GradientAnimator()
+    {
+
+        doc << GetToggleButton("Toggle");
+        doc << GetStepButton("Step");
+        doc << canvas;
+
+        cells.resize(num_w_boxes, std::vector<float>(num_h_boxes, 0));
+    }
+    void DoFrame()
+    {
+        canvas.Clear();
+
+        for (int x = 0; x < num_w_boxes; x++)
+        {
+            for (int y = 0; y < num_h_boxes; y++)
+            {
+                canvas.Rect(x * rect_len, y * rect_len, rect_len, rect_len, emp::ColorHSV(0, 0, 0.5), "black");
+            }
+        }
+
+        number = number + 0.1;
+        if (number > 1)
+        {
+            number = 0;
+        }
+        canvas.Rect(rect_len, rect_len, rect_len, rect_len, emp::ColorHSV(0, 0, number), "black");
+    }
+};
+
+GradientAnimator animator;
+
+int main()
+{
+    animator.Step();
+}
